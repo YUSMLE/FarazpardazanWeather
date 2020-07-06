2730bcb70cea7da0d562d0a126d35949300103ac
yusmle <behfar01@gmail.com>
2020-07-06 15:17:45 +0430

    Define open cities database

--- app/src/main/java/com/farazpardazan/weather/Const.kt
+++ app/src/main/java/com/farazpardazan/weather/Const.kt
@@ -5,6 +5,9 @@ object Const {
     // The base URL used for Open Weather API services.
     const val BASE_URL: String = "https://api.openweathermap.org/"
 
+    // The extra URL used for Open Cities API service.
+    const val OpenCities_URL: String = "https://my-json-server.typicode.com/YUSMLE/FarazpardazanWeather/"
+
     // The API Key required for Open Weather API services.
     const val API_KEY = "5e1e0769cd4f76cb00e9f302c35a69d1"
 
